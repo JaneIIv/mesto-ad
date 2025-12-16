08cfb4f29d110a1ccfb4dde218b7655cdf7b586c
Janellv <ievnenia@gmail.com>
2025-12-16 17:48:19 +0500

    общая разметка файла

---
+++ src/scripts/components/validation.js
@@ -0,0 +1,49 @@
+function showInputError() {
+
+}
+
+function hideInputError() {
+
+}
+
+function checkInputValidity() {
+  if () {
+    showInputError();
+  } else {
+    hideInputError();
+  }
+}
+
+function hasInvalidInput() {
+
+}
+
+function disableSubmitButton() {
+
+}
+
+function enableSubmitButton() {
+
+}
+
+function toggleButtonState() {
+  if () {
+    disableSubmitButton();
+  } else {
+    enableSubmitButton();
+  }
+}
+
+function setEventListeners(formElement, settings) {
+
+}
+
+function clearValidation() {
+
+}
+
+function enableValidation(settings) {
+
+}
+
+export {enableValidation, clearValidation}
