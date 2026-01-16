3f30754743c9231e3fc9ccbfa6fa863cd6f829d4
Jane <154761018+JaneIIv@users.noreply.github.com>
2026-01-16 21:24:04 +0500

    Create api.js

---
+++ src/scripts/components/api.js
@@ -0,0 +1,7 @@
+const config = {
+  baseUrl: "https://mesto.nomoreparties.co/v1/apf-cohort-202",
+  headers: {
+    authorization: "98fa7808-1b20-41e6-9d28-d59ecbdb695f",
+    "Content-Type": "application/json",
+  },
+};
